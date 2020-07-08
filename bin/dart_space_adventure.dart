92fdc337892fe492678839f85712b0349fabd5a1
Gregory Stula <gregstula@me.com>
2020-07-07 21:25:08 -0400

    Initial stub

--- bin/dart_space_adventure.dart
+++ bin/dart_space_adventure.dart
@@ -1,5 +1,27 @@
-import 'package:dart_space_adventure/dart_space_adventure.dart' as dart_space_adventure;
+import 'package:dart_space_adventure/dart_space_adventure.dart'
+    as dart_space_adventure;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_space_adventure.calculate()}!');
+  print('Welcome to the Solar System');
+  print('There are 8 planets to explore');
+  print('What is your name?');
+
+  var name = stdin.readLineSync();
+
+  print('Nice to meet you, $name!\n'
+      'Let\'s go on an adventure.');
+
+  print('Shall I randomly choose a planet for you to visit? (Y or N)');
+
+  var answer = stdin.readLineSync();
+
+  if (answer == 'Y') {
+    print('Ok, traveling to Mercury!\n'
+        'Arrived at Mercury. A very hot planet, closest to the sun.');
+  } else {
+    print('Name the planet you would like to visit.');
+    final planetName = stdin.readLineSync();
+    print('Traveling to $planetName');
+  }
 }
